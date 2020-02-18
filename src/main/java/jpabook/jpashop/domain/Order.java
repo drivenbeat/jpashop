5942c1ebe0403fb17b8452637d2a4e0216a4ccf6
jeong-min.kim <jeong-min.kim@jinisys.net>
2020-02-18 09:25:39 +0900

    LAZY 모드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -18,14 +20,14 @@ public class Order {
     private Long id;
 
     // 둘다 사용 (개발용) 인 경우 연관관계의 주인을 설정해야 하며 주로 fk가 가까이 있는 객체를 주인으로 함 (1:N 중의 N) 주인이 변경되는 쪽
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 연관테이블을 같이 묶어서 처리 (persist 설정하지 않을 경우 각각 해줘야 함)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
 
@@ -33,4 +35,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING) // enum 타입은 숫자로 넣지 말것
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // ============= 연관 관계 메서드 ===============
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrder().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
