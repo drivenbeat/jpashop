6dc824a3b2968b726c427c515ce0c8feff9d3906
JM <drivenbeat@gmail.com>
2020-03-03 17:29:49 +0900

    회원 등록 / 조회 view

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) { // binding result로 오류 전달 처리
+
+        // 에러가 있는 경우 해당 폼으로 이동 (결과와 함꼐)
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        // 복잡한 경우 dto 사용 - api 사용시에는 entity를 절대 그대로 사용하지 말것 (로직추가시에도 api 스펙이 변할 수 있음)
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
